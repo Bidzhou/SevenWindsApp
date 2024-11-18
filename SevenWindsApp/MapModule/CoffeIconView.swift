f1d7fc25d47e266176e3566b12fca046f05430f2
Bidzhou <89260226211@mail.ru>
2024-11-18 19:54:43 +0300

    Работает переход с карт в меню!

---
+++ SevenWindsApp/MapModule/CoffeIconView.swift
@@ -0,0 +1,20 @@
+//
+//  CoffeIconView.swift
+//  SevenWindsApp
+//
+//  Created by Frederico del' Bidzho on 17.11.2024.
+//
+
+import UIKit
+
+class CoffeIconView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
